25be6e43933dffbc1031f34e7aee90e8bf1370bf
d-taylor6403 <dannielletaylor@outlook.com>
2021-01-18 20:20:33 -0600

    adding fetch request

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,4 +1,19 @@
-var getUserRepos = function() {
-fetch('https://api.github.com/users/octocat/repos')};
+//brave browser fix
+ethereum.autoRefreshOnNetworkChange = false;
 
-getUserRepos();
+
+var getUserRepo = function(user) {
+    //format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+    //make a request to the url
+    fetch(apiUrl).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+        });
+    });
+};
+
+getUserRepo("d-taylor6403");
+
+
