4fe4dc0903a983f6802f5651fc74acf6495e593b
d-taylor6403 <dannielletaylor@outlook.com>
2021-01-22 19:15:26 -0600

    adding function to retrieve repo name from parameters

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,59 +1,94 @@
-let issueContainerEl = document.querySelector("#issues-container");
+ethereum.autoRefreshOnNetworkChange = false;
 
-//function to take in a repo name as a parameter
-let getRepoIssues = function(repo) {
-    console.log(repo);
-    let apiUrl = `https://api.github.com/repos/${repo}/issues?direction=asc`;
+var repoNameEl = document.querySelector("#repo-name");
+var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
 
+var getRepoIssues = function(repo) {
+    // format the github api url
+    var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
+
+    // make a get request to url
     fetch(apiUrl).then(function(response) {
-        //request was successful
+        // request was successful
         if (response.ok) {
             response.json().then(function(data) {
-                //pass response data to dom function
                 displayIssues(data);
+
+                // check if api has paginated issues
+                if (response.headers.get("Link")) {
+                    displayWarning(repo);
+                }
             });
-        }else{
-            alert('There was a problem with your request!');
+        }
+        else {
+            console.log(response);
+            alert("There was a problem with your request!");
         }
     });
 };
 
+getRepoName() {
+    let queryString = document.location.search;
+    let repoName = queryString.split("=")[1];
+    console.log(repoName);
+}
 
-let displayIssues = function(issues) {
-    //checking for issues on valid repo
+
+var displayIssues = function(issues) {
     if (issues.length === 0) {
-        issueContainerEl.textContent = 'This repo has no open issues!';
+        issueContainerEl.textContent = "This repo has no open issues!";
         return;
     }
-    for (let i = 0; i < issues.length; i++) {
-        //create a link element to take users to the issue on github
+
+    // loop over given issues
+    for (var i = 0; i < issues.length; i++) {
+        // create a link element to take users to the issue on github
         var issueEl = document.createElement("a");
         issueEl.classList = "list-item flex-row justify-space-between align-center";
         issueEl.setAttribute("href", issues[i].html_url);
         issueEl.setAttribute("target", "_blank");
 
-        //create span to hold issue title
-        let titleEl = document.createElement("span");
+        // create span to hold issue title
+        var titleEl = document.createElement("span");
         titleEl.textContent = issues[i].title;
 
-        //append to container
+
+        // append to container
         issueEl.appendChild(titleEl);
 
-        //create a type element
-        let typeEl = document.createElement("span");
+        // create a type element
+        var typeEl = document.createElement("span");
 
-        //check if issue is an actual issue or a pull request
+        // check if issue is an actual issue or a pull request
         if (issues[i].pull_request) {
-            typeEl.textContent = '(Pull Request)';
-        }else {
-            typeEl.textContent = '(Issue)';
+            typeEl.textContent = "(Pull request)";
+        }
+        else {
+            typeEl.textContent = "(Issue)";
         }
 
-        //append to container
+        // append to container
         issueEl.appendChild(typeEl);
+
+        // append to the dom
         issueContainerEl.appendChild(issueEl);
     }
 };
 
+var displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
 
-getRepoIssues("d-taylor6403/NewsScraper");
+    // create link element
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
+};
+
+getRepoIssues();
+getRepoName();
